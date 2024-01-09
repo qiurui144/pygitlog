eb12e916ac1ec8a48c238d2ff6bb63fe0ccaf1d4
qiurui <happyqiurui@163.com>
2024-01-09 17:14:37 +0800

    add avb support and fix some spell problems

--- pygitlog/bdanalysis.py
+++ pygitlog/bdanalysis.py
@@ -2,31 +2,38 @@
 import requests
 
 #获取kerywords的wiki url
-def get_wikipage(keywords):
-    if keywords == "md":
-        key = "mdadm"
-    elif keywords.startswith("PR"):
+def get_wikipage(keyword):
+    if keyword.startswith("PR"):
         return ""
-    else:
-        key = keywords
     S = requests.Session()
     URL = "https://en.wikipedia.org/w/api.php"
     PARAMS = {"action": "opensearch","namespace": "0","search": "","limit": "1","format": "json"}
-    PARAMS['search'] = key
+    PARAMS['search'] = keyword
     R = S.get(url=URL, params=PARAMS)
     DATA = R.json()
     #print(DATA)
     #print(DATA[3][0])
     return DATA[3][0]
 
+def get_true_keyword(keywords):
+    if keywords == "md":
+        keyword = "mdadm"
+    elif "avb" in keywords :
+        keyword = "Audio Video Bridging"
+
+    else:
+        keyword = keywords
+    return keyword
+
 #获取commit的关键词，如ext4
-def get_commit_kerywords(abbr):
+def get_commit_keyword(abbr):
     #print(abbr.split())
     for abb in abbr.split():
         if abb.endswith(":"):
             keywords = abb.replace(':','')
     for key in keywords.split('/'):
-        return "[" + key + "](" + get_wikipage(key) + ")"
+        keyword = get_true_keyword(key)
+        return "[" + keyword + "](" + get_wikipage(keyword) + ")"
 
 #获取commit的类型，如
 def get_commit_type(body):
