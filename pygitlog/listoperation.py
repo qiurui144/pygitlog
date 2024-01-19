7a41ea340cddc44bb190a47c90386f75bc9c6e2b
qiurui <happyqiurui@163.com>
2024-01-19 16:10:21 +0800

    add list operation function file

---
+++ pygitlog/listoperation.py
@@ -0,0 +1,13 @@
+
+def list_of_groups(init_list, childern_list_len):
+    '''
+    init_list为初始化的列表,childern_list_len初始化列表中的几个数据组成一个小列表
+    :param init_list:
+    :param childern_list_len:
+    :return:
+    '''
+    list_of_group = zip(*(iter(init_list),) *childern_list_len)
+    end_list = [list(i) for i in list_of_group]
+    count = len(init_list) % childern_list_len
+    end_list.append(init_list[-count:]) if count !=0 else end_list
+    return end_list
